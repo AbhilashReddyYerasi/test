ddb123069dba6a37164086b8f27456f8a91465ff
AbhilashReddyYerasi <a.yerasireddy@ezyhaul.com>
2024-05-23 13:58:49 +0530

    tt

---
+++ migrations/scripts/linting2.py
@@ -0,0 +1,45 @@
+import sys
+from schemachange.cli import JinjaTemplateProcessor, get_schemachange_config
+import sqlfluff
+
+def lint_changed_files(file_paths):
+    config = get_schemachange_config(
+        config_file_path="migrations/raw/schemachange-config.yml",
+        vars=None,
+        verbose=False,
+        root_folder=None,
+        modules_folder=None,
+        snowflake_account=None,
+        snowflake_user=None,
+        snowflake_role=None,
+        snowflake_warehouse=None,
+        snowflake_database=None,
+        change_history_table=None,
+        snowflake_schema=None,
+        create_change_history_table=None,
+        autocommit=None,
+        query_tag=None,
+        oauth_config=None,
+        dry_run=True
+    )
+
+    jinja_processor = JinjaTemplateProcessor(
+        project_root=config['root_folder'],
+        modules_folder=config['modules_folder'],
+    )
+
+    for file_path in file_paths:
+        print(file_path)
+        content = jinja_processor.render(jinja_processor.relpath(file_path), config['vars'], False) + ";\n"
+        res = sqlfluff.lint(
+            content,
+            dialect="snowflake",
+            config_path="/migrations/raw/.sqlfluff"
+        )
+        if res:
+            print("=================", file_path)
+            for r in res:
+                print(r["line_no"], r["description"], r)
+
+if __name__ == "__main__":
+    lint_changed_files(sys.argv[1:])
